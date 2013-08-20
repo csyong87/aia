d86d634705dc1b96f0e6dd02ba9afd2b4832fedb
Christian Javellana <cjjavellana@gmail.com>
2013-08-20 23:10:28 +0800

    Fixed error where username and password property got interchanged

--- aia-service/src/main/java/com/techmahindra/aia/service/security/UserDetailServiceImpl.java
+++ aia-service/src/main/java/com/techmahindra/aia/service/security/UserDetailServiceImpl.java
@@ -1,8 +1,8 @@
 package com.techmahindra.aia.service.security;
 
-import com.techmahindra.aia.model.RoleInfo;
-import com.techmahindra.aia.model.UserInfo;
-import com.techmahindra.aia.service.UserInfoService;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,15 +11,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.techmahindra.aia.model.RoleInfo;
+import com.techmahindra.aia.model.UserInfo;
+import com.techmahindra.aia.service.UserInfoService;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Christian
- * Date: 8/17/13
- * Time: 5:31 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: Christian Date: 8/17/13 Time: 5:31 PM To change this template use File | Settings |
+ * File Templates.
  */
 @Configurable
 public class UserDetailServiceImpl implements UserDetailsService {
@@ -45,6 +43,6 @@ public class UserDetailServiceImpl implements UserDetailsService {
             GrantedAuthority ga = new SimpleGrantedAuthority(String.format("ROLE_%s", roleInfo.getName()));
             authorities.add(ga);
         }
-        return new UserDetailsImpl(authorities, s, userInfo.getPassword(), true, true, true, true);
+        return new UserDetailsImpl(authorities, userInfo.getPassword(), s, true, true, true, true);
     }
 }
