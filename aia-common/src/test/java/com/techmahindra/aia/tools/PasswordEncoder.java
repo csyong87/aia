631afd7b437e04c0178bf6f2803c156f5ef838a2
Christian Javellana <cjjavellana@gmail.com>
2013-08-22 07:15:16 +0800

    Fix the error where passwords are encoded using SHA-1 instead of SHA-256

--- aia-common/src/test/java/com/techmahindra/aia/tools/PasswordEncoder.java
+++ aia-common/src/test/java/com/techmahindra/aia/tools/PasswordEncoder.java
@@ -5,7 +5,7 @@ import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 public class PasswordEncoder {
 
     public static void main(String... args) {
-        ShaPasswordEncoder pe = new ShaPasswordEncoder();
+        ShaPasswordEncoder pe = new ShaPasswordEncoder(256);
         pe.setIterations(1024);
         System.out.println(pe.encodePassword("password", "christian"));
 
--- support/sql/initial-inserts.sql
+++ support/sql/initial-inserts.sql
@@ -1,8 +1,8 @@
 --
-insert into USER_INFO (userinfoid, loginid, domain, username,
-  mobilenumber, password, email, emailpassword, status, createdby, datecreated)
-values (1, 'christian', 'msat', 'Christian', '####-####',
-  'f294bbd2135e1d5a7e8d6c683b7ba57bd10b6ebd',
-  'christian_javellana@xxxx.com', 'C1vEry80NvpRnAY+8PKw4CyJxkcDGRTZ',
-  'ACTIVE', 'ADMIN', sysdate);
-commit;
+INSERT INTO USER_INFO (userinfoid, loginid, domain, username,
+                       mobilenumber, password, email, emailpassword, status, createdby, datecreated)
+  VALUES (1, 'christian', 'msat', 'Christian', '####-####',
+          '64a3c0a664030e056fcc412a847022769139ff736bc58ad484f7809cfe127c4c',
+          'christian_javellana@xxxx.com', 'C1vEry80NvpRnAY+8PKw4CyJxkcDGRTZ',
+          'ACTIVE', 'ADMIN', sysdate);
+COMMIT;
