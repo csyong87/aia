5b664f1b35eba70db36444da9d0ec89fdee669c3
Christian Javellana <cjjavellana@gmail.com>
2013-09-01 22:26:34 +0800

    Added role & user_role into the initial inserts

--- support/sql/initial-inserts.sql
+++ support/sql/initial-inserts.sql
@@ -5,4 +5,18 @@ INSERT INTO USER_INFO (userinfoid, loginid, domain, username,
           '64a3c0a664030e056fcc412a847022769139ff736bc58ad484f7809cfe127c4c',
           'christian_javellana@xxxx.com', 'C1vEry80NvpRnAY+8PKw4CyJxkcDGRTZ',
           'ACTIVE', 'ADMIN', sysdate);
+          
+          
+-- create roles
+INSERT INTO ROLE_INFO (ROLEINFOID, CREATEDBY, DATECREATED, DATEUPDATED, DESCRIPTION, NAME, UPDATEDBY, VERSION) 
+	VALUES (1, 'ADMIN', sysdate, null, 'Administrator Role', 'ADMIN', null, 0);
+INSERT INTO ROLE_INFO (ROLEINFOID, CREATEDBY, DATECREATED, DATEUPDATED, DESCRIPTION, NAME, UPDATEDBY, VERSION) 
+	VALUES (2, 'ADMIN', sysdate, null, 'Manager Role', 'MANAGER', null, 0);
+INSERT INTO ROLE_INFO (ROLEINFOID, CREATEDBY, DATECREATED, DATEUPDATED, DESCRIPTION, NAME, UPDATEDBY, VERSION) 
+	VALUES (3, 'ADMIN', sysdate, null, 'Developer Role', 'DEVELOPER', null, 0);
+	
+-- Mapping between users and roles
+INSERT INTO USER_ROLE (USERROLEID, CREATEDBY, DATECREATED, DATEUPDATED, ROLEINFOID, STATUS, UPDATEDBY, USERINFOID,  VERSION) 
+	VALUES ( 1, 'ADMIN', sysdate, null, 1, 'ACTIVE', null, 1, 0);
+	
 COMMIT;
